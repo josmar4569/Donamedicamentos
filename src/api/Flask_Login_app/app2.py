285541b491828f16f136f049c5493eddb46a9612
jsosa26 <jesus.asm26@gmail.com>
2023-01-18 19:20:39 +0000

    Segundo commit del login

--- src/api/Flask_Login_app/app2.py
+++ src/api/Flask_Login_app/app2.py
@@ -2,5 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__'
-app.run(port = 3000, debug = True)
+@app.route('/')
+def Index():
+    return "Hello World"
+
+@app.route('/add_contact')
+def add_contact():
+    return "Agregar contacto"
+
+@app.route('/edit')
+def edit_contact():
+    return "Editar Contacto"
+
+@app.route('/Delete')
+def delete_contact():
+    return "Eliminar Contacto"
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
