32daace0c1abf5da63c1c26ba2fd99007487d79c
Josmar Manrique Andrew Yaguaran <manriquejosmar64@gmail.com>
2023-01-24 20:40:29 +0000

    Stop

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,8 +3,6 @@ import React, { Component } from "react";
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
     <p>      
-     © 2022 Copyright:
-      LIFEFORALL
     </p>
   </footer>
 );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,84 +6,99 @@ import "../../styles/home.css";
 export const Home = () => {
   const { store, actions } = useContext(Context);
   return (
-    <div className="row">
-      <div id="carouselExample" className="carousel slide">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <div className="card text-bg-dark">
-              <img src="rigo-baby.jpg" className="card-img d-block"></img>
-              <div className="card-img-overlay">
-                <h5 className="card-title text-primary">Card title</h5>
-                <p className="card-text text-primary">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+    <header className="container">
+      <div className="row">
+        <div id="carouselExampleCaptions" className="carousel slide">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="rigo-baby.jpg" className="d-block" alt="..."></img>
+              <div className="carousel-caption d-none d-md-block">
+                <h5 className="text-primary">Presentación</h5>
+                <p className="text-primary">
+                  Some representative placeholder content for the first slide.
                 </p>
-                <p className="card-text text-primary">
-                  <small>Last updated 3 mins ago</small>
+                <a href="/login" className="btn btn-primary btn-lg">
+                  Donar
+                </a>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src="rigo-baby.jpg" className="d-block" alt="..."></img>
+              <div className="carousel-caption d-none d-md-block">
+                <div>
+                  <h5 className="text-primary">Second slide label</h5>
+                  <p className="text-primary">
+                    Some representative placeholder content for the second
+                    slide.
+                  </p>
+                  <a href="/login" className="btn btn-primary">
+                    LOG IN TO YOUR ACCOUNT
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img src="rigo-baby.jpg" className="d-block" alt="..."></img>
+              <div className="carousel-caption d-none d-md-block">
+                <h5 className="text-primary">Third slide label</h5>
+                <p className="text-primary">
+                  Some representative placeholder content for the third slide.
                 </p>
+                <a href="/login" className="btn btn-primary">
+                  LOG IN TO YOUR ACCOUNT
+                </a>
               </div>
             </div>
           </div>
-          <div className="carousel-item">
-            <div className="card text-bg-dark">
-              <img src="rigo-baby.jpg" className="card-img d-block"></img>
-              <div className="card-img-overlay">
-                <h5 className="card-title text-primary">Card title</h5>
-                <p className="card-text text-primary">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p className="card-text text-primary">
-                  <small>Last updated 3 mins ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <div className="card text-bg-dark">
-              <img src="rigo-baby.jpg" className="card-img d-block"></img>
-              <div className="card-img-overlay">
-                <h5 className="card-title text-primary">Card title</h5>
-                <p className="card-text text-primary">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <p className="card-text text-primary">
-                  <small>Last updated 3 mins ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
-    </div>
+    </header>
   );
 };
 console.log(Home);
